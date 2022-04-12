1b95b9ac372198938d53c8f49acb3e228e286b99
Caioferrari04 <caioferrari0484@gmail.com>
2022-04-12 14:57:38 -0300

    adicionando projetobanco

---
+++ Conta.cs
@@ -0,0 +1,36 @@
+public class Conta
+{
+    public int Codigo { get; }
+    public double Saldo { get; private set; }
+
+    public Conta(int codigo)
+    {
+        Codigo = codigo;
+        Saldo = 0.0;
+        
+    }
+
+    public void Sacar (double valor)
+    {
+        Saldo = Saldo - valor;
+
+    if (valor > Saldo)
+    {
+        throw new ArgumentException ("Você não pode sacar um valor maior do que o saldo presente em sua conta");
+    }
+}
+    public void Depositar(double valor)
+    {
+        Saldo = Saldo + valor;
+    }
+    
+    public void Transferir(Conta conta, double valor)
+    {
+        if(Saldo >= valor)
+        {
+            Saldo = Saldo - valor;
+            conta.Saldo = conta.Saldo + valor;
+        }
+
+    }
+}
